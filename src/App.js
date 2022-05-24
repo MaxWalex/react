fc1093c06d1d86bac5755fe7b4fe3501df26deb1
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-05-24 22:35:14 +0300

    Props

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
+import Header  from "./components/Header"
+
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'}
-    ]
-
-    const load = false
-    const showComments = true
-
-    if (load) return <h1>Loading...</h1>
-
-    const commentBlock = (
-        <div className="comments">
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>
-    )
-
     return (
-        <div className="container">
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-            
-            {showComments && commentBlock}
-
-        </div>        
+        <>
+            <Header text='true'/>
+            <div className="container">
+                <h2>Feedback UI</h2>
+            </div>      
+        </>
     )
 }
 
