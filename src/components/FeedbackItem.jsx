aeba9e5578a51744213803740a42137807ff0f83
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-05-25 22:42:23 +0300

    write inside component card

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,24 +1,19 @@
-import {useState} from 'react'
+import { FaTimes } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from "./Shared/Card"
 
-function FeedbackItem() {
-
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState('This is an example a feedback item')
-
-    const handleClick = () => {
-        setRating((prev) => {
-            return prev + 1
-        })
-    }
-
+function FeedbackItem({ item, handleDelete }) {
   return (
-    <div className='card'>
-        <div className="num-display">{rating}</div>
-        <div className="text-display">{text}</div>
-        <button onClick={handleClick}>Click</button>
-
-    </div>
+    <Card>
+        <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className='close'><FaTimes color='purple' /></button>
+        <div className="text-display">{item.text}</div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
