e6aa352a78cc45c36f1f3be04a53660ca2e24d6c
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-05-25 23:34:03 +0300

    added FeedbackForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header  from "./components/Header"
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/feedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
         <>
             <Header text='Feedback UI' colorText="#f16969" />
             <div className="container">
+                <FeedbackForm />
                 <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback} handleDelete={id => deleteFeedback(id)} />
             </div>      
