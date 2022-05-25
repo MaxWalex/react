26e056bc4f78bf9dc903503578b2fa21821527fd
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-05-25 22:42:48 +0300

    added props & handleClick

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import FeedbackItem from "./FeedbackItem"
+
+function FeedbackList({ feedback, handleDelete }) {
+
+    if (!feedback || feedback.lenght === 0) {
+        return <p>No Feedback Yet</p>
+    }
+
+  return (
+    <div className="feedback_list">
+        {feedback.map((item) => {
+            return <FeedbackItem 
+            key={item.key} 
+            item={item} 
+            handleDelete={handleDelete} />
+        })}
+    </div>
+  )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired
+    })
+  )
+}
+
+export default FeedbackList
